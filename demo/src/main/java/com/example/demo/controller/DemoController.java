01c2c4e3ffb8968f07cf4282641abeb36620e441
h-fujinuma <fuji.2277339.sc@gmail.com>
2022-03-31 00:02:24 +0900

    スレッドセーフの確認追加

--- demo/src/main/java/com/example/demo/controller/DemoController.java
+++ demo/src/main/java/com/example/demo/controller/DemoController.java
@@ -10,9 +10,13 @@ public class DemoController {
     @Value("${env.var}")
     String envVar;
 
+    int threadSafeConfirmValue = 0;
+
     @GetMapping
     public String doGetDemoSample(){
-        return envVar;
+        threadSafeConfirmValue++;
+        // threadSafeConfirmValueはスレッド間で共有されるので、別々のリクエストAを送ってリクエストBを送るとthreadSafeConfirmValueは加算される
+        return envVar + "スレッドセーフの確認："+ threadSafeConfirmValue;
     }
     
 }
