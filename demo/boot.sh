888f6327a4f10dd5d54906000be999180ac87ab6
h-fujinuma <fuji.2277339.sc@gmail.com>
2022-05-05 00:39:47 +0900

    clamavインストールするシェル作成

--- demo/boot.sh
+++ demo/boot.sh
@@ -1,1 +1,26 @@
+echo "start: apt -y update"
+apt -y update
+echo "start: apt -y install clamav-daemon"
+apt -y install clamav-daemon
+echo "start edit clamd.conf"
+sed -i "s/User clamav/User root/" /etc/clamav/clamd.conf
+sed -i "s/LocalSocket \/var\/run\/clamav\/clamd.ctl/# LocalSocket \/var\/run\/clamav\/clamd.ctl/" /etc/clamav/clamd.conf
+sed -i "s/FixStaleSocket true/# FixStaleSocket true/" /etc/clamav/clamd.conf
+sed -i "s/LocalSocketGroup clamav/# LocalSocketGroup clamav/" /etc/clamav/clamd.conf
+sed -i "s/LocalSocketMode 666/# LocalSocketMode 666/" /etc/clamav/clamd.conf
+if grep -q TCPSocket /etc/clamav/clamd.conf; then \
+echo "TCPSocket already exists" ; \
+else \
+echo "TCPSocket 3310" >> /etc/clamav/clamd.conf ; \
+fi
+if grep -q TCPAddr /etc/clamav/clamd.conf; then \
+echo "TCPAddr already exists" ; \
+else \
+echo "TCPAddr 127.0.0.1" >> /etc/clamav/clamd.conf ; \
+fi
+echo "satrt: freshclam"
+freshclam
+echo "satrt: clamd"
+clamd
+echo "start: spring"
 java -jar app.jar
