0df4629ef4b01a482b8b49e9c9f02cd4a993e1f9
h-fujinuma <fuji.2277339.sc@gmail.com>
2022-05-12 15:04:32 +0900

    設定ファイルをコピーするように修正

--- demo/boot.sh
+++ demo/boot.sh
@@ -1,22 +1,10 @@
 #!/bin/bash
 
-CLAMD_CONF="/etc/clamav/clamd.conf"
+# 設定ファイルの上書き
+mv -f /clamd.conf /etc/clamav
+mv -f /freshclam.conf /etc/clamav
+chown -R clamav:clamav /etc/clamav
 
-echo "start edit clamd.conf"
-sed -i "s/LocalSocket \/var\/run\/clamav\/clamd.ctl/# LocalSocket \/var\/run\/clamav\/clamd.ctl/" $CLAMD_CONF
-sed -i "s/FixStaleSocket true/# FixStaleSocket true/" $CLAMD_CONF
-sed -i "s/LocalSocketGroup clamav/# LocalSocketGroup clamav/" $CLAMD_CONF
-sed -i "s/LocalSocketMode 666/# LocalSocketMode 666/" $CLAMD_CONF
-if grep -q TCPSocket $CLAMD_CONF; then \
-    echo "TCPSocket already exists" ; \
-else \
-    echo "TCPSocket 3310" >> $CLAMD_CONF ; \
-fi
-if grep -q TCPAddr $CLAMD_CONF; then \
-    echo "TCPAddr already exists" ; \
-else \
-    echo "TCPAddr 127.0.0.1" >> $CLAMD_CONF ; \
-fi
 echo "start: freshclam"
 freshclam
 echo "start: clamd"
